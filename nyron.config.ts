cd03f52f484a130cdb04eb183de56a4f8df9837a
#V0ID <hey@v0id.me>
2025-10-20 11:26:59 +0300

    chore: Update Nyron configuration to include 'coin' project with version 0.1.1 and adjust package metadata

--- nyron.config.ts
+++ nyron.config.ts
@@ -3,13 +3,9 @@ import { defineConfig } from "@nyron/cli/config"
 export default defineConfig({
   repo: "v0id-user/coin",
   projects: {
-    sdk: {
-      tagPrefix: "@my-package/sdk@",
-      path: "packages/sdk",
-    },
-    service: {
-      tagPrefix: "@my-package/service@",
-      path: "apps/service",
+    coin: {
+      tagPrefix: "coin@",
+      path: "__version__/",
     },
   },
   autoChangelog: true,
